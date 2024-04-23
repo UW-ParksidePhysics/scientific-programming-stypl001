dc827cfc116dcf84d30360a6c3b6d93c8ed982be
Stypl001 <158517083+Stypl001@users.noreply.github.com>
2024-04-23 15:10:55 -0500

    Update table_temperature_conversion_with_while.py

--- loops_and_lists/table_temperature_conversion_with_while.py
+++ loops_and_lists/table_temperature_conversion_with_while.py
@@ -1,1 +1,16 @@
+import numpy as np
 
+import math as m
+
+fahrenheit = 0
+superscript_o = '\u2070'
+
+print("{:<8} {:<15}".format(superscript_o + "F", superscript_o +"C"))
+
+while fahrenheit <= 100:
+  celsius = (fahrenheit - 32) * 5/9
+  print("{:<8} {:<15}".format(fahrenheit, round(celsius,2)))
+  fahrenheit += 10
+  
+
+  
