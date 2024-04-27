ceb4c63deeeaa09d6f67102d61cd72e9abf7e248
Stypl001 <158517083+Stypl001@users.noreply.github.com>
2024-04-27 13:57:17 -0500

    Update convert_fahrenheit_temperature_to_celsius_from_command_line.py

--- User Input and Error Handling/convert_fahrenheit_temperature_to_celsius_from_command_line.py
+++ User Input and Error Handling/convert_fahrenheit_temperature_to_celsius_from_command_line.py
@@ -1,1 +1,29 @@
+#with replit we do not have a command line, so we can not use the input function.
 
+
+import numpy as np
+import sys
+import math as m
+
+def convert_celsius_temperature_to_fahrenheit(celsius_temperature):
+  return (9. / 5) * celsius_temperature + 32
+
+def convert_fahrenheit_temperature_to_celsius(fahrenheit_temperature):
+  return (5. / 9) * (fahrenheit_temperature - 32)
+
+try:
+    fahrenheit = float(input('Enter a temperature in Fahrenheit: '))
+except ValueError:
+    fahrenheit = float(input('You must enter a number!:'))
+    print(f'{fahrenheit} Fahrenheit converts to {convert_fahrenheit_temperature_to_celsius(fahrenheit):.1f} celsius')
+
+
+
+
+else:    
+    print(f'{fahrenheit} Fahrenheit converts to {convert_fahrenheit_temperature_to_celsius(fahrenheit):.1f} celsius')
+
+
+if __name__ == '__main__':
+  if len(sys.argv) == 2 and sys.argv[1] == 'test':
+    test_all_functions()
