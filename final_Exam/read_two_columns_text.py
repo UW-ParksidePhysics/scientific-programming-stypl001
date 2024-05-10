da1c7198292d4e2f0a108345bb447a2aa55314e5
Stypl001 <158517083+Stypl001@users.noreply.github.com>
2024-05-10 10:39:01 -0500

    Create read_two_columns_text.py

---
+++ final_Exam/read_two_columns_text.py
@@ -0,0 +1,34 @@
+'''Takes in a dat file and returns a 2D array of the data.'''
+
+'''__i.stypla__'''
+
+import numpy as np
+import pandas as pd
+
+def read_two_columns_text(filename):
+  
+  '''This will read in data from a file into a 2D array'''
+  data= []
+  M = 0 #this is the number of data points in the table
+
+  try:  
+    #read table in and put headers x & y for the two cloumns
+    data = pd.read_table(filename, delimiter= ' ', header= None)
+    data.columns = ['x', 'y']
+    #find the number of records in the table
+    M = data.shape[0]
+  except IOError:
+    #if there is no file with the name in str then   print the error message
+    print(f'File {filename} not found')
+
+  return data, M
+
+'''This is the test area'''
+if __name__ == "__main__":
+
+  str = 'final_exam_review/volumes_energies.dat'
+  data, M = read_two_columns_text(str)
+
+  print(f'{data=}, shape={data.shape}')
+
+  
